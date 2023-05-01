13c62b7f752386eb4f8857c5bdbf35aa188b468e
Harshita <harshitaagarwal15@gmail.com>
2023-05-01 12:33:19 +0530

    commented s3 code

--- EKS-Infra-State-Bucket/bucket.tf
+++ EKS-Infra-State-Bucket/bucket.tf
@@ -18,31 +18,31 @@ provider "aws" {
   region = "us-west-2"
 }
 
-# Create S3 bucket to hold the terraform state files
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "test-c360-dev-terraform"
+# # Create S3 bucket to hold the terraform state files
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "test-c360-dev-terraform"
 
-  tags = {
-    Category    = "360_Core"
-    SubCategory = "Innovation"
-  }
-}
+#   tags = {
+#     Category    = "360_Core"
+#     SubCategory = "Innovation"
+#   }
+# }
 
-resource "aws_s3_bucket_public_access_block" "terraform_state_block_access" {
-  bucket = aws_s3_bucket.terraform_state.id
+# resource "aws_s3_bucket_public_access_block" "terraform_state_block_access" {
+#   bucket = aws_s3_bucket.terraform_state.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encrypt" {
-  bucket = aws_s3_bucket.terraform_state.bucket
+# resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encrypt" {
+#   bucket = aws_s3_bucket.terraform_state.bucket
 
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "aws:kms"
-    }
-  }
-}
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "aws:kms"
+#     }
+#   }
+# }
