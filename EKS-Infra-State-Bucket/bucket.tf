6911ccefaec2bdb8eabb65b31b6315d8eba5dbfa
Harshita <harshitaagarwal15@gmail.com>
2023-05-01 11:42:04 +0530

    added provider code in bucket.tf

--- EKS-Infra-State-Bucket/bucket.tf
+++ EKS-Infra-State-Bucket/bucket.tf
@@ -1,3 +1,24 @@
+terraform {
+  required_version = "= 1.4.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "eks-cluster-remote-state"
+    key            = "EKS-Infra-State-Bucket/terraform.tfstate"
+    encrypt        = true
+    region         = "us-west-2"
+  }
+ }
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-west-2"
+}
+
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "eks-cluster-remote-state"
 
